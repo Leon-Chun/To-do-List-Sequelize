44eafaa1f72c2c2344e51c8ad1a160481f30913d
Leon-Chun <s1235753@gmail.com>
2022-11-18 18:39:36 +0800

    feat: add views and template

--- app.js
+++ app.js
@@ -22,6 +22,27 @@ app.get('/', (req, res) => {
   res.send('This is my test page for initialization(初始化， 成功) ')
 })
 
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`Express is listening on localhost:${PORT}`)
 })
