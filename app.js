21262add817bf0e321adfe22553399bef45adf7c
Leon-Chun <s1235753@gmail.com>
2022-11-21 21:51:53 +0800

    modify finish this project

--- app.js
+++ app.js
@@ -1,77 +1,45 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const session = require('express-session')
+const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
+
+// 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
-//是載入一包 Passport 設定檔
 
-const PORT = process.env.PORT || 3000
-const passport = require('passport')
-//是把 Passport 套件本身載入進來，給路由用
-
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
-
-
-//使用套件後產生的
 const app = express()
+const PORT = process.env.PORT
 
-//view engine
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-//user body get 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(session({
-  secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
-usePassport(app)
-
-//router setting
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
+usePassport(app) // 載入Passport 設定檔
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
-  console.log(`Express is listening on localhost:${PORT}`)
+  console.log(`App is running on http://localhost:${PORT}`)
 })
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+//底下小路由們
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/user')
+const auth = require('./modules/auth')
+
+const { authenticator } = require('../middleware/auth')
+
+
+//請求 與 對應小路由。 路由必須由複雜開始往下排
+router.use('/todos', authenticator, todos)
+router.use('/users', users)
+router.use('/auth', auth)
+router.use('/', authenticator, home)
+
+module.exports = router
+//依照收到的網址請求，總路由丟出相應檔案過去
+//意思總路由有全檔案路線，而個別資料引用則寫在各路由設定下。
